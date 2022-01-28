effb61ca4492ce41aac7927cbf0210f935739b9a
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2022-01-28 09:40:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth } from "@frontegg/react";
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+
+  console.log("*** authed user ****", user);
+
+  const mainStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const linkStyles = {
+    margin: "2rem",
+  };
+
+  const imageStyles = {
+    borderRadius: "50%",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isAuthenticated ? (
+        <div style={mainStyles}>
+          <p>Successfully authenticated</p>
+          <img
+            style={imageStyles}
+            src={user.profilePictureUrl}
+            alt={user.name}
+          />
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Email Verified: {user.email_verified ? "Yes" : "No"}</p>
+          <p>
+            Phone Number:{" "}
+            {user.phoneNumber ? user.phoneNumber : "Not available"}
+          </p>
+          <p>Sign in provider: {user.provider}</p>
+          <p>Is user verified: {user.verified ? "Yes" : "No"}</p>
+          <Link style={linkStyles} to="/account/logout">
+            Logout
+          </Link>
+        </div>
+      ) : (
+        <div style={mainStyles}>
+          <p>hello, you're not authenticated</p>
+          <Link style={linkStyles} to="/account/sign-up">
+            Signup
+          </Link>
+          <Link style={linkStyles} to="/account/login">
+            Login
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FronteggProvider } from "@frontegg/react";
+
+// this should be added to doppler
+const contextOptions = {
+  baseUrl: "https://app-lzdkz9b7n4m2.frontegg.com",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FronteggProvider contextOptions={contextOptions}>
+      <App />
+    </FronteggProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
